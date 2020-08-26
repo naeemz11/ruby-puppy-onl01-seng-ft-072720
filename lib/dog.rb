1d548d7a559f0b4f011515904ccc6a021c9a7449
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-26 15:36:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 # Add your code here
 
+<<<<<<< HEAD
 class Dog
 
   attr_accessor :name
@@ -32,3 +33,38 @@ class Dog
   end
 
 end
+=======
+class Dog 
+  
+  attr_accessor :name 
+  
+  @@all= []
+  
+  
+  def initialize(name)
+    @name= name 
+    self.save
+  end 
+  
+  def save
+    @@all << save
+  end
+    
+  
+  def self.all
+    @all
+  end 
+  
+  def self.print_all
+    @@all.each do |dog|
+    puts dog.name 
+  end 
+end 
+  
+  def self.clear_all
+    @@all= []
+  end 
+  
+  
+end 
+>>>>>>> 658a48807d645fc2d8b2cfc577f6e445853d3f64
